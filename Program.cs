c748418f539198f7f63c2183559d84e00e90e1b3
Alumno <Alumno@PC-B02>
2023-10-10 10:27:48 -0600

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿//PROGRAMA AVES BALBUENA
+using System;
+using System.Collections.Generic;
+class program
+{
+    static void Main()
+    {
+        List<string> aves = new List<string> { "Loro gris", "paloma de diamante", "coctel" };
+        Console.WriteLine("los valores de la lista antes de insertar:");
+        foreach (string ave in aves)
+        {
+            Console.Write(ave + "");
+        }
+        Console.WriteLine();
+        aves.Add("mayna");
+        aves.Add("periquitos");
+        aves.Add("cactua");
+
+        Console.WriteLine("los valores de la lista despues de insertar:");
+
+        foreach (string ave in aves)
+        {
+            Console.Write(ave + "");
+        }
+        Console.WriteLine();
+    }
+}
